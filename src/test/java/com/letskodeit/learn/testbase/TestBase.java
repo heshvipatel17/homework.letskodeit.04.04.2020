01f59be46c40f1ef1caa08c9461d7c5c9d49e13b
Heshvipatel17 <heshvipatel17@gmail.com>
2020-04-14 01:25:05 +0100

    home work

--- src/test/java/com/letskodeit/learn/testbase/TestBase.java
+++ src/test/java/com/letskodeit/learn/testbase/TestBase.java
@@ -2,6 +2,7 @@ package com.letskodeit.learn.testbase;
 
 import com.letskodeit.learn.basepage.BasePage;
 import com.letskodeit.learn.browserselector.BrowserSelector;
+import com.letskodeit.learn.loadproperty.LoadProperty;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
@@ -9,20 +10,25 @@ import java.util.concurrent.TimeUnit;
 
 public class TestBase extends BasePage {
 
+    //object creation
+    LoadProperty loadProperty = new LoadProperty();
     BrowserSelector browserSelector = new BrowserSelector();
-    String baseUrl = "https://learn.letskodeit.com/";
 
+    //getting "baseUrl" & "browser" values from config.properties
+    String baseUrl = loadProperty.getProperty("baseUrl");
+    String browser = loadProperty.getProperty("browser");
 
-    @BeforeMethod
+    //open browser
+    @BeforeMethod(groups = {"Regression", "Sanity", "Smoke"})
     public void openBrowser() {
-        browserSelector.selectBrowser("chrome");
+        browserSelector.selectBrowser(browser);
         driver.manage().window().maximize();
 //        driver.manage().timeouts().pageLoadTimeout(40, TimeUnit.SECONDS);
         driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
         driver.get(baseUrl);
     }
-
-    @AfterMethod
+    //close browser
+    @AfterMethod(groups = {"Regression", "Sanity", "Smoke"})
     public void tearDown() {
         driver.quit();
 
--- src/test/java/com/letskodeit/learn/testsuite/TestCase.java
+++ src/test/java/com/letskodeit/learn/testsuite/TestCase.java
@@ -9,22 +9,22 @@ import org.testng.annotations.Test;
 public class TestCase extends TestBase {
     HomePage homepage = new HomePage();
 
-    @Test
+    @Test(priority = 0, groups = {"Sanity","Regression"})
     public void verifyUserShouldClickOnLoginLink() {
         homepage.clickOnLoginLink();
     }
 
-    @Test
+    @Test(priority = 1, groups = {"Sanity","Regression"})
     public void verifyUserShouldClickOnPracticeLink() {
         homepage.clickOnPracticeLink();
     }
 
-    @Test
+    @Test(priority = 2, groups = {"Smoke","Regression"})
     public void verifyUserShouldClickOnSignUpLink() {
         homepage.clickOnSignUpLink();
     }
 
-    @Test
+    @Test(priority = 3, groups = {"Smoke","Regression"})
     public void verifyUserShouldGetWelcomeText() {
         homepage.getWelcomeText();
         String expectedText = "Welcome to Let's Kode It";
